238fb766501a43d00c09fda7da02582c70f21c71
Daniel Mittereder <danielmittereder@gmail.com>
2023-05-12 00:52:01 +0200

    Draw a square

--- main.mjs
+++ main.mjs
@@ -1,19 +1,87 @@
+// https://codelabs.developers.google.com/your-first-webgpu-app
+
 const adapter = await navigator.gpu.requestAdapter();
 const device = await adapter.requestDevice();
 
 const canvas = document.querySelector("canvas");
 const context = canvas.getContext("webgpu");
-const format = navigator.gpu.getPreferredCanvasFormat();
-context.configure({ device, format })
+const canvasFormat = navigator.gpu.getPreferredCanvasFormat();
+context.configure({ device, format: canvasFormat })
+
+const vertices = new Float32Array([
+  // X,    Y,
+  -0.8, -0.8, // Triangle 1 (Blue)
+   0.8, -0.8,
+   0.8,  0.8,
+
+  -0.8, -0.8, // Triangle 2 (Red)
+   0.8,  0.8,
+  -0.8,  0.8,
+]);
+
+const vertexBuffer = device.createBuffer({
+  label: 'Cell vertices',
+  size: vertices.byteLength,
+  usage: GPUBufferUsage.VERTEX | GPUBufferUsage.COPY_DST,
+});
+
+device.queue.writeBuffer(vertexBuffer, 0, vertices);
+
+/**
+ * @type { GPUVertexBufferLayout }
+ */
+const vertexBufferLayout = {
+  arrayStride: 8,
+  attributes: [{
+    format: "float32x2",
+    offset: 0,
+    shaderLocation: 0, // Position, see vertex shader
+  }],
+};
+
+const cellShaderModule = device.createShaderModule({
+  label: "Cell shader",
+  code: `
+    @vertex
+    fn vertexMain(@location(0) pos: vec2f) -> @builtin(position) vec4f {
+      return vec4f(pos, 0, 1);
+    }
+
+    @fragment
+    fn fragmentMain() -> @location(0) vec4f {
+      return vec4f(0.5, 0.5, 0, 1);
+    }
+    `
+});
+
+const cellPipeline = device.createRenderPipeline({
+  label: 'Cell pipeline',
+  layout: 'auto',
+  vertex: {
+    module: cellShaderModule,
+    entryPoint: 'vertexMain',
+    buffers: [vertexBufferLayout],
+  },
+  fragment: {
+    module: cellShaderModule,
+    entryPoint: 'fragmentMain',
+    targets: [{ format: canvasFormat }]
+  }
+});
 
 const encoder = device.createCommandEncoder();
-encoder.beginRenderPass({
+const pass = encoder.beginRenderPass({
     colorAttachments: [{
         view: context.getCurrentTexture().createView(),
         loadOp: "clear",
-        clearValue: { r: 0, g: 0, b: 1, a: 1 },
+        clearValue: { r: 0, g: 0.5, b: 0.5, a: 1 },
         storeOp: "store",
       }]
-}).end()
+})
+
+pass.setPipeline(cellPipeline);
+pass.setVertexBuffer(0, vertexBuffer);
+pass.draw(vertices.length / 2);
+pass.end()
 
 device.queue.submit([encoder.finish()]);
